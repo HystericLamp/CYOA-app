4a5c78c0fb4c1cacb949da6ef6d1a68174268261
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2025-05-15 17:47:53 -0600

    Setting up Tailwindcss and page
    Will do Styling later

---
+++ cyoa-front/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <h1>
+                        Create your own Adventure
+                    </h1>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ cyoa-front/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div class="max-w-screen-lg mx-auto">
+        <h2 class="text-3xl font-bold underline">
+            Hello World
+        </h2>
+    </div>
+  );
+};
+
+export default Home;
